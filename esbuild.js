c9a900c8e92575345ef5f17c934706d9f6871443
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-09-30 15:14:10 +0800

    chore: clean up code

--- esbuild.js
+++ esbuild.js
@@ -1,20 +1,16 @@
 import esbuild from "esbuild";
 import peerDependencies from "./package.json" assert { type: "json" };
 
-const sharedConfig = {
-  entryPoints: ["./src/index.ts"],
-  bundle: true,
-  minify: false,
-  sourcemap: false,
-  target: "esnext",
-  tsconfig: "./tsconfig.json",
-  // Exclude the token.json
-  external: Object.keys(peerDependencies),
-};
-
 esbuild
   .build({
-    ...sharedConfig,
+    entryPoints: ["./src/index.ts"],
+    bundle: true,
+    minify: false,
+    sourcemap: false,
+    target: "esnext",
+    tsconfig: "./tsconfig.json",
+    // Exclude the token.json
+    external: ["./src/scripts/*", ...Object.keys(peerDependencies)],
     format: "esm",
     outfile: "./build/index.esm.js",
     target: ["esnext", "node12"],
